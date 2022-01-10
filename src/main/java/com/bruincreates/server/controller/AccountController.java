b145183e4715fe84571874f6b9c8c698099d067d
Ao Jiao <aojiao@Aos-MacBook-Pro.local>
2022-01-09 19:21:38 -0800

    Email App Credential Updated

--- src/main/java/com/bruincreates/server/controller/AccountController.java
+++ src/main/java/com/bruincreates/server/controller/AccountController.java
@@ -6,7 +6,6 @@ import com.bruincreates.server.model.servlet.RestResponse;
 import com.bruincreates.server.model.user.UserControlBlock;
 import com.bruincreates.server.service.AccountService;
 import com.bruincreates.server.utility.JwtUtil;
-import com.bruincreates.server.utility.ServletUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
