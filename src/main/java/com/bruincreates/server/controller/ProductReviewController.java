f700abf0b05431a511754d29b054b7fdb3852e54
Molly Wehrenberg <mwehrenberg@g.ucla.edu>
2022-02-01 17:34:06 -0800

    Add add/delete methods to product review service

--- src/main/java/com/bruincreates/server/controller/ProductReviewController.java
+++ src/main/java/com/bruincreates/server/controller/ProductReviewController.java
@@ -1,6 +1,7 @@
 package com.bruincreates.server.controller;
 
 import com.bruincreates.server.model.response.RestResponse;
+import com.bruincreates.server.model.user.UserControlBlock;
 import com.bruincreates.server.service.ProductReviewService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.bruincreates.server.utility.JwtUtil;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.security.Principal;
+import com.bruincreates.server.dao.po.*;
 
 @Slf4j
 @RestController
@@ -30,6 +36,21 @@ public class ProductReviewController {
         //TODO: implementation needed
         //TODO: 3 reviews are loaded everytime
         return RestResponse.success();
+    }
+
+    @PostMapping("/addReview")
+    public RestResponse<String> addReview(Product prod, String review) {
+        UserControlBlock user = (UserControlBlock) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username = user.getUsername();
+        Integer id = Integer.parseInt(prod.getProductId());
+        productReviewService.addReview(username, id, review);
+        return RestResponse.success("Product Review Added");
+    }
+
+    @PostMapping("/deleteReview")
+    public RestResponse<String> delteReview(ProductReview review) {
+        productReviewService.deleteReview(review);
+        return RestResponse.success("Product Review Deleted");
     }
 
 }
--- src/main/java/com/bruincreates/server/service/ProductReviewService.java
+++ src/main/java/com/bruincreates/server/service/ProductReviewService.java
@@ -3,11 +3,30 @@ package com.bruincreates.server.service;
 import com.bruincreates.server.dao.mapper.ProductReviewMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.bruincreates.server.dao.po.ProductReview;
+import com.bruincreates.server.exception.BadRequestException;
 
 @Service
 public class ProductReviewService {
 
     @Autowired
     ProductReviewMapper productReviewMapper;
+
+    public void addReview(String username, Integer productId, String review){
+        //check if the caller has purchased current product and if the current status is completed
+        //create review only if not written one already
+        ProductReview prodReview = new ProductReview();
+        prodReview.setReviewer(username);
+        prodReview.setId(productId);
+        prodReview.setReview(review);
+        productReviewMapper.insert(prodReview);
+        
+    }
+
+    public void deleteReview(ProductReview review){
+        //check if the review exists and if the caller if the original buyer
+        Integer id = review.getId();
+        productReviewMapper.deleteByPrimaryKey(id);
+    }
 
 }
