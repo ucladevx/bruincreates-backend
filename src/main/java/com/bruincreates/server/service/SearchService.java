e2a1f13e785f431def7a00e0a2eba126b902dbb7
Ao Jiao <aojiao@Aos-MacBook-Pro.lan>
2022-02-01 19:25:55 -0800

    SearchService Functions Defined

--- src/main/java/com/bruincreates/server/service/SearchService.java
+++ src/main/java/com/bruincreates/server/service/SearchService.java
@@ -1,8 +1,23 @@
 package com.bruincreates.server.service;
 
+import com.bruincreates.server.dao.po.Product;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SearchService {
+
+    //TODO: functions called by ProductService to sync MySql data with Elasticsearch
+    //e.g. ProductService.java
+    //          task: insert a new product
+    //          ProductMapper.insertSelective(someProduct); //insert to MySql
+    //          SearchService.createDocument(someProduct);  //insert to Elasticsearch
+
+    public void createDocument(Product product) {}
+
+    public void updateDocument(Product product) {}
+
+    public void deleteDocument(Product product) {}
+
+    //TODO: functions for your search logic below:
 
 }
